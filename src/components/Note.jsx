c9e8f2e8e2f082c4fd0542f94425d4f95a818798
Onix <gotnix13@gmail.com>
2020-01-04 13:02:19 -0400

    Added a data folder, to store a mock array of notes

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Note(){
+    return <div class="note">
+        <h1>Title of the note boiii</h1>
+        <p>This is the body of the nobe foo'</p>
+    </div>
+}
+
+export default Note;
