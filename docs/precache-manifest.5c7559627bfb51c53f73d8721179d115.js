df69a79d21a75e2ca553d6aa4d83f0bfa06cfb86
Onix <gotnix13@gmail.com>
2020-01-05 20:43:10 -0400

    added a docs folder

---
+++ docs/precache-manifest.5c7559627bfb51c53f73d8721179d115.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fadcedf28b8ca736e9eacfead37deea",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "7474f436b46063380475",
+    "url": "/keeper/static/js/2.3c0c586c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/keeper/static/js/2.3c0c586c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba96652792297ecd19e0",
+    "url": "/keeper/static/js/main.4fa709ea.chunk.js"
+  },
+  {
+    "revision": "c3b5ada28727506ea087",
+    "url": "/keeper/static/js/runtime-main.3f9ae201.js"
+  }
+]);
